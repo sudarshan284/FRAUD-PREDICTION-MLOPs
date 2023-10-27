d38a94c150664dbf05cc1902d3ad3fde4682b0c0
Ashish Kumar <81232686+ashishk831@users.noreply.github.com>
2023-10-27 18:47:58 +0530

    Update data_upload_operator.py

--- dags/operators/data_upload_operator.py
+++ dags/operators/data_upload_operator.py
@@ -1,5 +1,4 @@
 from azure.storage.blob import BlobServiceClient
-#from config.constant import storage_account_key, storage_account_name, connection_string, container_name, file_path_up, file_name
 
 storage_account_key = "+w6iadskKBmzsPTwmDpkkXsuCcJ2XFcW2cmuh8fcAvXSFNVLOk2UpPwqWc2WMYNbDRT/aRqz+44c+ASt1yRMMA=="
 storage_account_name = "cardstorage2"
@@ -23,6 +22,5 @@ def uploadToBlobStorage():
 
 uploadToBlobStorage()
 
-#uploadToBlobStorage(file_path_up,file_name)
 
 
