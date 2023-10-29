5fb085a230aa05b9d3f28cd133f958907dd9ca48
Ashish Kumar <81232686+ashishk831@users.noreply.github.com>
2023-10-29 15:50:40 +0530

    Add files via upload

---
+++ dags/operators/config/constant.py
@@ -0,0 +1,9 @@
+storage_account_key = "azure_storage_key"
+storage_account_name = "azure_storage_account_name"
+connection_string = "azure_storage_account_connection_string"
+container_name = "azure_storage_container_name"
+file_path_up = "/location_to_upload_file/Train1.csv"
+file_name = "Train11"
+blob_name="Train11"
+file_path_down="/location_to_download_file/blobfile.csv"
+
